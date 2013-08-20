90fb1fe8f4b64640efa8e4eb3cff2e18a0757c86
ZackMance <Zack.Mance@summa-tech.com>
2013-08-19 21:29:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZmDotCom::Application.config.secret_token = '3a1ab7490ce3fa9ede4e8219f61b9a56892a1e638ab0fd0743e44195e33a5f8ed7b80a32a9d0356c669423d61d4b039be9f7262e57cfdc5e4fdc93cc60cd633c'
